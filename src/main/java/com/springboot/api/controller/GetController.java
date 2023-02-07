3e93dc01e50a82e23f37308e3d413b8a1d81bb86
seung9526 <95rus@naver.com>
2023-02-08 07:14:14 +0900

    spring-boot-api study_3

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,8 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.boot.env.ConfigTreePropertySource;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,11 +32,12 @@ public class GetController {
         return var;
     }
 
+    @ApiOperation(value = "GET 메서드 예제", notes = "@RequstParam을 활용한 GET Method")
     @GetMapping(value = "/request1")
     public String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String organization){
+            @ApiParam(value = "이름", required = true) @RequestParam String name,
+            @ApiParam(value = "이메일", required = true) @RequestParam String email,
+            @ApiParam(value = "회사", required = true) @RequestParam String organization){
         return name + " " + email + " " + organization;
     }
 
