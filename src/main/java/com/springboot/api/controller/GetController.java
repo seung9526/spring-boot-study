7aa0b47e130d34a004b1a61d168d04b2604cddba
seung9526 <95rus@naver.com>
2023-02-08 07:52:08 +0900

    spring-boot-api study_3

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -3,7 +3,8 @@ package com.springboot.api.controller;
 import com.springboot.api.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.boot.env.ConfigTreePropertySource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -12,18 +13,23 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String getHello(){
-        return "Hellow world";
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
+        return "Hello world";
     }
 
     @GetMapping(value = "/name")
     public String getName(){
+        LOGGER.info("getName 메서드가 호출되었습니다.");
         return "Flature";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {} ", variable);
         return variable;
     }
 
